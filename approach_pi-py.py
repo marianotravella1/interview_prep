ee5ecc760b32f29490267070501b369cba29f643
marianotravella <marianotravella2@gmail.com>
2026-01-10 17:09:42 -0300

    Time measures added in function

--- approach_pi-py.py
+++ approach_pi-py.py
@@ -1,7 +1,8 @@
 # pi = 4 * (1 - 1/3 + 1/5 - 1/7 + 1/9 - ...)
     
-    
+import time
 def approach_pi(n_terms):
+    start_time = time.time()
     pi_quarter = 0
     sign = 1
     divisor = 1
@@ -10,8 +11,13 @@ def approach_pi(n_terms):
         pi_quarter += sign * (1 / divisor)
         sign *= -1  
         divisor += 2
+    
+    aproximation = 4 * pi_quarter
+    end_time = time.time()
+    print(f'Aproximation of PI: {aproximation}')
+    print(f'Time: {end_time - start_time} seconds.')
         
-    return 4 * pi_quarter
+    return aproximation
     
 
-print(approach_pi(100000000))
+approach_pi(100000000)
