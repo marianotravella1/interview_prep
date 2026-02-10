e46affa53de162e553dd5831b91544ded5691a99
Mariano <marianotravella2@gmail.com>
2026-02-10 19:20:04 -0300

    Add implementation for sqrt()

---
+++ problems/69._sqrt(x)/solution.py
@@ -0,0 +1,45 @@
+"""
+LeetCode Problem: 69. Sqrt(x)
+Link: https://leetcode.com/problems/sqrtx/submissions/1915175779/
+
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+"""
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+
+        left, right = 1, x // 2
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return right
+
+if __name__ == '__main__':
+    sol = Solution()
