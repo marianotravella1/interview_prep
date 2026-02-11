d27b268a4427359f44939e090a31bd51d64bd84f
Mariano <marianotravella2@gmail.com>
2026-02-11 15:56:56 -0300

    Add implementation for climbing stairs problem

---
+++ problems/70._climbing_stairs/solution.py
@@ -0,0 +1,53 @@
+"""
+LeetCode Problem: 70. Climbing Stairs
+Link: https://leetcode.com/problems/climbing-stairs/description/
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+
+Constraints:
+
+1 <= n <= 45
+"""
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        first, second = 1, 2
+
+        for _ in range(3, n + 1):
+            current = first + second
+            first, second = second, current
+
+        return second
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.climbStairs(2))  # 2
+    print(sol.climbStairs(3))  # 3
+    print(sol.climbStairs(8))  # 34
+    print(sol.climbStairs(5))  # 8
+    print(sol.climbStairs(45))  # 1836311903
