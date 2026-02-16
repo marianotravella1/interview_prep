9b3bdd47d46600d8615456d4a58caafcaf27b90a
Mariano <marianotravella2@gmail.com>
2026-02-15 22:30:29 -0300

    solution for longest valid parenthesis and search in rotated sorted array

---
+++ problems/32._longest_valid_parentheses/solution.py
@@ -0,0 +1,52 @@
+"""
+LeetCode Problem: 32. Longest Valid Parentheses
+Link: https://leetcode.com/problems/longest-valid-parentheses/description/
+
+Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
+
+
+
+Example 1:
+
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+Example 2:
+
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()".
+Example 3:
+
+Input: s = ""
+Output: 0
+
+
+Constraints:
+
+0 <= s.length <= 3 * 104
+s[i] is '(', or ')'.
+"""
+
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = [-1]
+        max_len = 0
+
+        for i in range(len(s)):
+            if s[i] == "(":
+                stack.append(i)
+            else:
+                stack.pop()
+                if len(stack) == 0:
+                    stack.append(i)
+                else:
+                    max_len = max(max_len, i - stack[-1])
+
+        return max_len
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.longestValidParentheses(')(()()(()(()()()()()(()()()()()()()))))'))
---
+++ problems/33._search_in_rotated_sorted_array/solution.py
@@ -0,0 +1,69 @@
+"""
+LeetCode Problem: 33. Search in Rotated Sorted Array
+Link: https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+All values of nums are unique.
+nums is an ascending array that is possibly rotated.
+-104 <= target <= 104
+"""
+
+
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+
+        while start <= end:
+            mid = start + (end - start) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[mid] >= nums[start]:
+                if target >= nums[start] and target < nums[mid]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            else:
+                if target > nums[mid] and target <= nums[end]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+
+        return -1
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(f"Test Case 1: {sol.search([4,5,6,7,0,1,2], 0)}") # Expected: 4
+    print(f"Test Case 2: {sol.search([4,5,6,7,0,1,2], 3)}") # Expected: -1
+    print(f"Test Case 3: {sol.search([1], 0)}")             # Expected: -1
